4214be6585f6459ad45036ac7f70a305496dbc4a
Vitali Vishneuski <Vitali_Vishneuski@epam.com>
2019-11-14 14:55:32 +0300

    add ALA-05 template

---
+++ ALA-05/SumApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SumApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            do
+            {
+                Console.WriteLine("Введите целое число:");
+                if (int.TryParse(Console.ReadLine(), out var integer))
+                {
+                    double sum = 0;
+                    for (var number = 0; number <= integer; number++)
+                    {
+                        sum += number;
+                    }
+                    Console.WriteLine($"Сумма целых чисел от 0 до {integer} = {(int)sum}");
+                }
+            } while (true);
+        }
+    }
+}
